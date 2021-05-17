a3ea9fa6b843c9159ee83ddfeb8b5e9fad3cba69
guzmanrafael <rafael.guzman1609@gmail.com>
2021-05-17 13:21:59 -0700

    make it better

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,11 +4,7 @@ const { UserRules, Validation, AuthMiddleware } = require("../middlewares");
 module.exports = function ({ AuthController }) {
   const router = Router();
 
-  router.post(
-    "/signup",
-    [AuthMiddleware, UserRules(), Validation],
-    AuthController.signUp
-  );
+  router.post("/signup", [UserRules(), Validation], AuthController.signUp);
   router.post("/signin", [UserRules(), Validation], AuthController.signIn);
 
   return router;
